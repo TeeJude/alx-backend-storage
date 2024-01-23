8b9c9e5454c50b08006bd90911c2262322a3b555
TeeJude <teejayjude@gmail.com>
2024-01-23 13:44:00 -0800

    101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module containing function that returns all students sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """Function that returns all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
