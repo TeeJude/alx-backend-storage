9a8efd15ba46585ec63deb1bc51f8c6f78b061f7
TeeJude <teejayjude@gmail.com>
2024-01-23 12:37:06 -0800

    9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,4 +4,5 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """Function that inserts a new document"""
-    return mongo_collection.insert_one(kwargs).insert_id
+    new_doc = mongo_collection.insert_one(kwargs).insert_id
+    return new_doc
