76bd5249a4013d6e9479617ed89d9cb7d87b7a64
TeeJude <teejayjude@gmail.com>
2024-01-23 13:26:03 -0800

    11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""Module that contains function that returns the list of school"""
+"""
+Module that contains function that returns the list of school
+"""
 import pymongo
 
 def schools_by_topic(mongo_collection, topic):
     """
     function that returns topic
     """
-    return mongo_collection.find("topics": topic))
+    return mongo_collection.find({"topics": topic})
